5279799ce259767f20ab3c1c9653fdb247ef555f
Marat <115820948+DEHALIBOCH@users.noreply.github.com>
2023-10-03 22:11:26 +0600

    Chat routes
    * Chat routes

---
+++ src/main/kotlin/kz/dehaliboch/routes/ChatRoutes.kt
@@ -0,0 +1,53 @@
+package kz.dehaliboch.routes
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.consumeEach
+import kz.dehaliboch.room.MemberAlreadyExistsException
+import kz.dehaliboch.room.RoomController
+import kz.dehaliboch.sessions.ChatSession
+
+fun Route.chatSocket(roomController: RoomController) {
+    webSocket(path = "/chat-socket") {
+        val session = call.sessions.get<ChatSession>()
+        if (session == null) {
+            close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "No session"))
+            return@webSocket
+        }
+        try {
+            roomController.onJoin(
+                username = session.username,
+                sessionId = session.sessionId,
+                socket = this
+            )
+            incoming.consumeEach { frame ->
+                if (frame is Frame.Text) {
+                    roomController.sendMessage(
+                        senderUsername = session.username,
+                        message = frame.readText(),
+                    )
+                }
+            }
+        } catch (e: MemberAlreadyExistsException) {
+            call.respond(HttpStatusCode.Conflict)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            roomController.tryDisconnect(session.username)
+        }
+    }
+}
+
+fun Route.getAllMessages(roomController: RoomController) {
+    get("/messages") {
+        call.respond(
+            status = HttpStatusCode.OK,
+            message = roomController.getAllMessages()
+        )
+    }
+}
