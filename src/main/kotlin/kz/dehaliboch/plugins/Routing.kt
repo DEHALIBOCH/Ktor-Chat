6dec5b72bfa747f6e337df1aded9cee081b024f1
marat <almabekov.marat@gmail.com>
2023-10-03 22:22:43 +0600

    Chat routes

--- src/main/kotlin/kz/dehaliboch/plugins/Routing.kt
+++ src/main/kotlin/kz/dehaliboch/plugins/Routing.kt
@@ -1,6 +1,16 @@
 package kz.dehaliboch.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import kz.dehaliboch.room.RoomController
+import kz.dehaliboch.routes.chatSocket
+import kz.dehaliboch.routes.getAllMessages
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
+    val roomController by inject<RoomController>()
+    install(Routing) {
+        chatSocket(roomController)
+        getAllMessages(roomController)
+    }
 }
