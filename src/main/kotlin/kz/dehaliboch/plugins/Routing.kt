5fc332b45eb3c92a36dc6eb94019fb4f01523746
marat <almabekov.marat@gmail.com>
2023-10-03 18:07:05 +0600

    Removing unnecessary routings

--- src/main/kotlin/kz/dehaliboch/plugins/Routing.kt
+++ src/main/kotlin/kz/dehaliboch/plugins/Routing.kt
@@ -5,9 +5,4 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-    }
 }
