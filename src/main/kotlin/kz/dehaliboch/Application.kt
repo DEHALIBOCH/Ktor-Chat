45f9d159dfab54a9176ca7777b56f1ddefc9bc4c
marat <almabekov.marat@gmail.com>
2023-10-03 22:18:23 +0600

    Koin di

--- src/main/kotlin/kz/dehaliboch/Application.kt
+++ src/main/kotlin/kz/dehaliboch/Application.kt
@@ -1,14 +1,20 @@
 package kz.dehaliboch
 
 import io.ktor.server.application.*
+import io.ktor.server.application.*
 import kz.dehaliboch.common.DatabaseHelper.configureDatabaseVariables
+import kz.dehaliboch.di.mainModule
 import kz.dehaliboch.plugins.*
+import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(Koin) {
+        modules(mainModule)
+    }
     configureSockets()
     configureSerialization()
     configureMonitoring()
