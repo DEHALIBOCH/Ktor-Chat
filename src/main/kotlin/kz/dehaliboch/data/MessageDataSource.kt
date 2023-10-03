10ffbdabc154b4dce64e34fba478a9742230c372
marat <almabekov.marat@gmail.com>
2023-10-03 21:26:43 +0600

    MessageDataSource

--- src/main/kotlin/kz/dehaliboch/data/MessageDataSource.kt
+++ src/main/kotlin/kz/dehaliboch/data/MessageDataSource.kt
@@ -1,10 +1,11 @@
 package kz.dehaliboch.data
 
 import kz.dehaliboch.data.model.Message
+import kz.dehaliboch.data.model.MessageDraft
 
 interface MessageDataSource {
 
     fun getAllMessages() : List<Message>
 
-    fun insertMessage(message: Message)
+    fun insertMessage(message: MessageDraft): Int
 }
