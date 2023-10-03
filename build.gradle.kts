bec1cf880c4ccee5a40cf86f3869b389462e3f5a
marat <almabekov.marat@gmail.com>
2023-10-03 18:07:21 +0600

    Additional dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
-
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val slf4j_version: String by project
+val ktorm_version: String by project
+val koin_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.10"
@@ -36,4 +37,13 @@ dependencies {
     implementation("org.slf4j:slf4j-api:$slf4j_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // ktorm
+    implementation("org.ktorm:ktorm-core:$ktorm_version")
+    implementation("org.ktorm:ktorm-support-mysql:$ktorm_version")
+
+    // koin
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 }
